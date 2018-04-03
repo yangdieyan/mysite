cc0a5bc6eb481d9fc8ac539e804a8dc601d2f878
ymt <ymt18868126640@outlook.com>
2018-04-03 16:20:08 +0800

    update content

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -10,6 +12,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -1,57 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.template import loader
-
 
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
 
 
-from .models import Question, Choice
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions(not including
+        those set to be published in the future."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    template = loader.get_template('polls/index.html')
-#    context = {
-#        'latest_question_list': latest_question_list,
-#    }
-#    return HttpResponse(template.render(context, request))
-
-# Leave the rest of the views (detail, results, vote) unchanged
-
-
-# ...
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-#def detail(request, question_id):
-#    try:
-#        question = Question.objects.get(pk=question_id)
-#    except Question.DoesNotExist:
-#        raise Http404("Question does not exist")
-#    return render(request, 'polls/detail.html', {'question': question})
-   # return HttpResponse("You're looking at question %s." % question_id)
-
-
-#def results(request, question_id):
-#    response = "You're looking at the results of question %s."
-#    return HttpResponse(response % question_id)
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
