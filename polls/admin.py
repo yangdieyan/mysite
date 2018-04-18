26953a32621652378adaca2460bca68e676e24ea
ymt <ymt18868126640@outlook.com>
2018-04-18 10:16:47 +0800

    gitignore

--- polls/admin.py
+++ polls/admin.py
@@ -14,8 +14,12 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {
+                            'classes': ('collapse',),
+                            'fields': ['pub_date']
+                            }),
     ]
+
     inlines = [ChoiceInline]
 
     list_display = ('question_text', 'pub_date', 'was_published_recently')
